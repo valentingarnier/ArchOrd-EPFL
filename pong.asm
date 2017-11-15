d51bdd532748d5e64241681a04df61aa238834b3
Orchampt-Mareschal Maina Patricia <orchampt@intranet.epfl.ch>
2017-11-15 13:22:22 +0100

    adding pong hit test

--- pong.asm
+++ pong.asm
@@ -5,16 +5,16 @@
 .equ BUTTONS, 0x2030 ; Button addresses
 
 ;BEGIN: main
-	addi a0, zero, 4
-	addi a1, zero, 3
-	addi t1, zero, 1
-	addi t2, zero, 1
+	addi a0, zero, 2
+	addi a1, zero, 1
+	addi t1, zero, -1
+	addi t2, zero, -1
 	stw a0, BALL(zero)
 	stw a1, BALL+4(zero)
 	stw t1, BALL+8(zero)
 	stw t2, BALL+12(zero)
 
-	addi t3, zero, 3
+	addi t3, zero, 2
 	addi t4, zero, 3
 	stw t3, PADDLES(zero)
 	stw t4, PADDLES+4(zero)
@@ -26,10 +26,10 @@ main:
 	ldw a1, BALL+4(zero)
 
 	call set_pixel
-	call hit_test
-	call move_ball
-	call move_paddles
 	call draw_paddles
+	call hit_test
+	call move_paddles
+	call move_ball
 	br main
 ;END: main
 
@@ -67,22 +67,100 @@ hit_test:
 
 	ldw t6, PADDLES(zero) ; position y du paddle 1
 	ldw t7, PADDLES+4(zero) ; position y du paddle 2
-
 	
-
-	cmpeqi t4, t0, 0
-	cmpeqi t5, t0, 11
-	or t4, t4, t5 ; si on est sur un des deux bords verticaux
-	beq t4, zero, test_y ; si rien a changer, alors go tester y
-	sub t2, zero, t2 ; pour partir d'un bord vertical on change juste la coordonnee x de la velocity
-	stw t2, BALL+8(zero)	
-test_y:
+	addi v0, zero, 0
+	
+test_bord_horizontal:
 	cmpeqi t4, t1, 0
 	cmpeqi t5, t1, 7
 	or t4, t4, t5 ; si on est sur un des deux bords horizontaux
-	beq t4, zero, hit_test_ret ; si rien a changer, alors return
-	sub t3, zero, t3 ; pour partir d'un bord vertical on change juste la coordonnee x de la velocity
+	beq t4, zero, test_bord_gauche ; si rien a changer, alors return
+	sub t3, zero, t3 ; pour partir d'un bord horizontal on change juste la coordonnee y de la velocity
 	stw t3, BALL+12(zero)
+
+test_bord_gauche:
+	cmpeqi t4, t0, 1
+	beq t4, zero, test_bord_droite
+	beq t1, t6, hit_paddle_front ; si on est en face des trois cases
+	addi t6, t6, 1
+	beq t1, t6, hit_paddle_front
+	addi t6, t6, -2
+	beq t1, t6, hit_paddle_front
+	
+	add t0, t0, t2 ; futur pos ball x
+	add t1, t1, t3 ; futur pos ball y
+
+
+
+
+	cmpeq t4, t1, t6 ; si notre futur y est sur le y du paddle
+	cmpeqi t5, t0, 0 ; si notre futur x est sur le x du paddle
+	
+	sub t0, t0, t2
+	sub t1, t1, t3 ; on retourne dans le hit paddle alors qu'il faut pas
+	
+	beq t4, zero, test_partie_inferieur_paddle_1 ; si notre futur y n'y est pas on se barre
+	beq t4, t5, velocity_inverse ; si la position est bien ce qu'on veut on inverse la velocity
+	addi v0, v0, 2 	
+	br hit_test_ret
+	
+	;l'autre x maintenant
+test_bord_droite:
+	cmpeqi t4, t0, 10
+	beq t4, zero, hit_test_ret
+	beq t1, t7, hit_paddle_front
+	addi t7, t7, 1
+	beq t1, t7, hit_paddle_front
+	addi t7, t7, -2
+	beq t1, t7, hit_paddle_front
+
+	add t0, t0, t2
+	add t1, t1, t3
+	cmpeq t4, t1, t7
+	cmpeqi t5, t0, 10
+
+	beq t4, zero, test_partie_inferieur_paddle_2
+	beq t4, t5, velocity_inverse
+	addi v0, v0, 1
+	br hit_test_ret
+	
+
+
+test_partie_inferieur_paddle_1:
+	addi t6, t6, 2
+	cmpeq t4, t1, t6 ; si notre futur y est sur le paddle
+	cmpeqi t5, t0, 0 ; si notre futur x est sur notre paddle
+	beq t4, zero, winner2 ; si notre futur y n'y est pas on se barre
+	beq t4, t5, velocity_inverse ; si la position est bien ce qu'on veut on inverse la velocity	
+
+test_partie_inferieur_paddle_2:
+	addi t7, t7, 2
+	cmpeq t4, t1, t7 ; si notre futur y est sur le paddle
+	cmpeqi t5, t0, 11 ; si notre futur x est sur notre paddle
+	beq t4, zero, winner1 ; si notre futur y n'y est pas on se barre
+	beq t4, t5, velocity_inverse ; si la position est bien ce qu'on veut on inverse la velocity	
+
+winner1:
+		addi v0, v0, 1
+		br hit_test_ret
+
+winner2:
+	addi v0, v0, 2
+	br hit_test_ret
+
+velocity_inverse:
+	sub t2, zero, t2
+	stw t2, BALL+8(zero)
+	sub t3, zero, t3
+	stw t3, BALL+12(zero)
+	br test_bord_horizontal
+;des qu'on change la velocite il faut checker si la nouvelle velocite ne hit pas un wall
+	
+hit_paddle_front:
+	sub t2, zero, t2
+	stw t2, BALL+8(zero)
+	br hit_test_ret
+		
 hit_test_ret:
 	ret
 ;END: hit_test
